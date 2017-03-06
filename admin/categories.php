bf857d1c22306882bf9f64cb279181b81ab11b56
Vasily Bezruchkin <vbezruchkin@intelliants.com>
2017-03-06 14:03:48 +0600

    Plugin ready for release.

--- admin/categories.php
+++ admin/categories.php
@@ -2,7 +2,7 @@
 /******************************************************************************
  *
  * Subrion - open source content management system
- * Copyright (C) 2016 Intelliants, LLC <http://www.intelliants.com>
+ * Copyright (C) 2017 Intelliants, LLC <https://intelliants.com>
  *
  * This file is part of Subrion.
  *
@@ -20,7 +20,7 @@
  * along with Subrion. If not, see <http://www.gnu.org/licenses/>.
  *
  *
- * @link http://www.subrion.org/
+ * @link https://subrion.org/
  *
  ******************************************************************************/
 
@@ -51,14 +51,6 @@ class iaBackendController extends iaAbstractControllerModuleBackend
 	protected function _indexPage(&$iaView)
 	{
 		$iaView->grid('_IA_URL_modules/faq/js/admin/categories');
-	}
-
-	protected function _setPageTitle(&$iaView)
-	{
-		if (in_array($iaView->get('action'), array(iaCore::ACTION_ADD, iaCore::ACTION_EDIT)))
-		{
-			$iaView->title(iaLanguage::get('faq_categ_' . $iaView->get('action')));
-		}
 	}
 
 	protected function _setDefaultValues(array &$entry)
